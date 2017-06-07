a41bcd5ea71bf072b0c099172f1f5d44c3b6b4cd
siva831997 <sivanandhu.ece97@gmail.com>
2017-06-07 09:49:17 +0530

    Add files via upload

---
+++ New.java
@@ -0,0 +1,12 @@
+package ope;
+
+public class New {
+
+	public static void main(String[] args) {
+		String number = "10";
+		Integer result = Integer.valueOf(number);
+		System.out.println(result);
+
+	}
+
+}
